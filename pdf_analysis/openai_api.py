4951eb9a6f5176dfadce547496def8e234d9c8e3
Liu Tao <liutaw@sjtu.edu.cn>
2024-05-14 15:44:53 +0800

    update latest analysis

--- pdf_analysis/openai_api.py
+++ pdf_analysis/openai_api.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import tiktoken
 
 class OpenAIClient:
-    def __init__(self, api_key, default_url="https://api.openai.com", model_name="gpt-4-turbo"):
+    def __init__(self, api_key, default_url="https://api.openai.com", model_name="gpt-4o-2024-05-13"):
 
         self.model_name = model_name
         self.client = OpenAI(api_key=api_key, base_url=default_url + '/v1')
@@ -12,7 +12,7 @@ class OpenAIClient:
 
 
         response = self.client.chat.completions.create(
-            model=,
+            model=self.model_name,
             messages=[
                 {
                     "role": "user",
